c1bc2af9798307a6ac3711c019713a2595ef260c
T. Scott Barnes <barnes.t.scott@gmail.com>
2012-11-21 21:23:03 -0500

    Added all the themes from Bootswatch.

--- bootstrap_themes/templatetags/bootstrap_themes.py
+++ bootstrap_themes/templatetags/bootstrap_themes.py
@@ -5,6 +5,6 @@ register = template.Library()
 
 @register.simple_tag
 def bootstrap_theme(theme):
-    if not theme:
+    if not theme or theme == '':
         return staticfiles_storage.url('bootstrap/css/bootstrap.min.css')
     return staticfiles_storage.url('bootstrap/themes/%(theme)s/css/%(theme)s.min.css' % dict(theme=theme))
