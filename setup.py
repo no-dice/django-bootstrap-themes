ec34d20c471344a1ef2618acc939c308a2dd05f8
T. Scott Barnes <barnes.t.scott@gmail.com>
2013-11-22 15:03:47 -0500

    Updated Bootswatch themes, including adding the new Yeti theme

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@
 
 from setuptools import setup, find_packages
 
-version = '3.0.3'
+version = '3.0.5'
 
 setup(
     name='django-bootstrap-themes',
--- update_bootswatch.py
+++ update_bootswatch.py
@@ -6,19 +6,25 @@ theme_api = urllib2.urlopen('http://api.bootswatch.com/3/')
 data = json.load(theme_api)
 for theme in data['themes']:
     less_path = os.path.abspath(os.path.join(os.path.dirname(__file__), 'bootstrap_themes', 'static', 'bootstrap', 'themes', theme['name'].lower(), 'less'))
+    if not os.access(less_path, os.F_OK):
+        os.makedirs(less_path)
     if os.access(less_path, os.W_OK):
-        if os.access(os.path.join(less_path, 'bootswatch.less'), os.W_OK):
+        try:
             theme_less = urllib2.urlopen(theme['less'])
             theme_less_file = open(os.path.join(less_path, 'bootswatch.less'), 'w')
             theme_less_file.write(theme_less.read())
             theme_less_file.close()
             theme_less.close()
-        if os.access(os.path.join(less_path, 'variables.less'), os.W_OK):
+        except IOError:
+            print "Writing to file " + os.path.join(less_path, 'bootswatch.less') + " failed!"
+        try:
             theme_less = urllib2.urlopen(theme['lessVariables'])
             theme_less_file = open(os.path.join(less_path, 'variables.less'), 'w')
             theme_less_file.write(theme_less.read())
             theme_less_file.close()
             theme_less.close()
+        except IOError:
+            print "Writing to file " + os.path.join(less_path, 'variables.less') + " failed!"
 theme_list_file = open(os.path.abspath(os.path.join(os.path.dirname(__file__), 'bootstrap_themes', 'themes.py')), 'w')
 theme_list_file.write('theme_data = ')
 theme_list_file.write(json.dumps(data))
