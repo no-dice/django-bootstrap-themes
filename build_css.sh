abdfa18aad9ecd4f48c39cc77d85a4c768ebe963
T. Scott Barnes <barnes.t.scott@gmail.com>
2014-05-18 12:34:49 -0400

    Fixed CSS build script

--- build_css.sh
+++ build_css.sh
@@ -6,7 +6,7 @@ cd bootstrap_themes/static/bootstrap/themes
 for theme in *; do
   if [ -d "$theme" ]; then
     mkdir -p "$theme/css"
-    recess --compile "$theme/less/bootstrap.less" > "$theme/css/bootstrap.css"
-    recess --compile --compress "$theme/less/bootstrap.less" > "$theme/css/bootstrap.min.css"
+    lessc "$theme/less/bootstrap.less" > "$theme/css/bootstrap.css"
+    recess --compress "$theme/css/bootstrap.css" > "$theme/css/bootstrap.min.css"
   fi
 done
